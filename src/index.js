dab84b3e9a20af81e901b3c568e6250a1695ed9e
Katriaryna Mazgo <ekaterina.mozgo@mail.ru>
2018-09-29 09:39:33 +0300

    smart-calculator/fix: implemented class SmartCalculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.result = initialValue;
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.result += " + " + number;
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.result += " - " + number;
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.result += " * " + number;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        this.result += " / " + number;
+        return this;
+    }
+
+    pow(number) {
+        this.result += "**" + number;
+        return this;
+    }
+
+    valueOf() {
+        return eval(this.result);
+    }
 }
 
 module.exports = SmartCalculator;
